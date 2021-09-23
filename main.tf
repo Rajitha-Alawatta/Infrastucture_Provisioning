5f85c06c9416d8c2e5868ffb4368a5fb0b33df9c
Rajitha-Alawatta <rajithaalawatta@gmail.com>
2021-09-23 11:09:47 +0530

    Updated

---
+++ main.tf
@@ -0,0 +1,84 @@
+provider "google" {
+  project     = "${var.gcp_project_id}"
+  region      = "${var.region}"
+  credentials = "${file("terraform.json")}"
+  #version = "~> 1.60.1"
+}
+
+## GCP Instance Template ##
+
+resource "google_compute_instance_template" "test_instance_template" {
+  name        = "${var.instance_template}"
+  description = "This template is used to create test server instances."
+
+  labels = {
+    environment = "${var.env}"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "${var.vm_type}"
+
+  scheduling {
+    automatic_restart   = true
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image      = "${var.image_name}"
+  }
+
+  network_interface {
+    network = "${var.nic}"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  #metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = <<SCRIPT
+  ${file("${var.startup_script}")}
+  SCRIPT
+
+  
+  metadata = {
+    environment = "${var.data}"
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "${var.sa_email}"
+    scopes = ["cloud-platform"]
+  }
+}
+
+
+## GCP Instance Group ## 
+
+resource "google_compute_instance_group_manager" "test_instance_group" {
+  name = "${var.group_name}"
+
+  base_instance_name = "${var.vm_name}"
+  zone               = "${var.zone}"
+
+  version {
+  instance_template  = google_compute_instance_template.test_instance_template.id
+  }
+
+  target_size  = 2
+}
+
+
+## AutoHealing ##
+
+#resource "google_compute_health_check" "autohealing" {
+#  name                = "${var.autohealing_name}"
+#  check_interval_sec  = 5
+#  timeout_sec         = 5
+#  healthy_threshold   = 2
+#  unhealthy_threshold = 10 # 50 seconds
+
+#  http_health_check {
+#    request_path = "/healthz"
+#    port         = "8080"
+#  }
---
+++ vars.tf
@@ -0,0 +1,55 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "gcp_project_id" {
+    default = "test-environment-262811"
+}
+
+variable "vm_name" {
+  default = "test-vm"
+}
+
+variable "vm_type" {
+    default = "e2-small"
+}
+
+variable "zone" {
+    default = "us-central1-a"
+}
+
+variable "image_name" {
+    default = "centos-cloud/centos-7"
+}
+
+variable "nic" {
+    default = "default"
+}
+
+variable "instance_template" {
+    default = "test-template"
+}
+
+variable "env" {
+    default = "test"
+}
+
+variable "data" {
+    default = "test"
+}
+
+variable "sa_email" {
+    default = "terraform@test-environment-262811.iam.gserviceaccount.com"
+}
+
+variable "group_name" {
+    default = "test-group"
+}
+
+variable "autohealing_name" {
+    default = "gcp_autohealer"
+}
+
+variable "startup_script" {
+    default = "startup_script.sh"
+}
